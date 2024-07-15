2bb0bb5b601492b748c83b5da60f61392aa85147
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2024-07-15 13:37:07 +0300

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['react-native-reanimated/plugin'],
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@app': './src/App.tsx',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@screens': './src/screens',
+          '@images': './src/assets/images/index',
+          '@svg': './src/assets/svg',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
